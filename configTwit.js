d52301ca7830ad37309f2038982d4eaa5ad3f7bf
Eyewais <80709639+Eyewais@users.noreply.github.com>
2023-03-03 21:56:49 +0500

    First Commit

---
+++ configTwit.js
@@ -0,0 +1,8 @@
+module.exports = {
+    appKey: 'LpZ10MyS8cmzInkZsxb52IhNU',
+    appSecret: 'tBJeztphgfSX2fRFqQtfMRF5YaHM52a1cYVKsx1ejskywJAFLd',
+    accessToken: '1610783044620754944-AdWerjzPDlUMwJ03bmQrkQyFG2dLsK',
+    accessSecret: 'BF72Cl5P9l99twIWnYDncmAmTbBaklczRbjaFXPOnbNsx',
+    timeout_ms: 60 * 1000,  // optional HTTP request timeout to apply to all requests.
+    //strictSSL:            true,     // optional - requires SSL certificates to be valid.
+}
---
+++ index.js
@@ -0,0 +1,185 @@
+var axios = require("axios").default;
+console.log('Follow Alert Bot!');
+const { TwitterApi } = require('twitter-api-v2');
+var config = require('./configTwit');
+const client = new TwitterApi(config);
+var CronJob = require('cron').CronJob;
+
+var job = new CronJob('0 * * * *', function () {
+    let date = new Date();
+    var offset = -300; //Timezone offset for EST in minutes.
+    var estDate = new Date(date.getTime() + offset * 60 * 1000);
+
+    if (estDate.getDay() == 1 && estDate.getHours() == 9) { //Monday 9AM EST
+        processIPOs();
+    }
+    if (isLastDayOfMonth(estDate) && estDate.getHours() == 9) { //Every Last Day Of the Month
+        processWithdarawls();
+    }
+    if (estDate.getHours() == 9) { //Every day at 9AM EST
+        publicCompanies();
+    }
+});
+job.start();
+
+async function processIPOs() {
+    const date = getDate();
+    const IPOs = await getIPOdata(date);
+
+    let tweetID;
+    if (IPOs.length > 0) {
+        tweetID = await makeTweet(`Here are this weeks upcoming IPO’s🧵\nTotal: ${IPOs.length} `, false);
+    }
+    else {
+        console.log('No IPOs to tweet about');
+        return;
+    }
+    for (let IPO of IPOs) {
+        let tweet = `Symbol: ${IPO.proposedTickerSymbol}
+Company: ${IPO.companyName}
+Exchange: ${IPO.proposedExchange}
+Price: ${IPO.proposedSharePrice} USD
+Shares: ${IPO.sharesOffered}
+Expected IPO Date: ${IPO.expectedPriceDate}
+Offer Amount: ${IPO.dollarValueOfSharesOffered} `;
+        tweetID = await makeTweet(tweet, true, tweetID);
+    }
+}
+
+async function processWithdarawls() {
+    const date = getDate();
+    const withdrawns = await getWithdarawlsdata(date);
+
+    let tweetID;
+    if (withdrawns.length > 0) {
+        tweetID = await makeTweet(`Here are this months IPO withdraws🧵\nTotal: ${withdrawns.length} `, false);
+    }
+    else {
+        console.log('No Withdarawls to tweet about');
+        return;
+    }
+    for (let wtd of withdrawns) {
+        let tweet = `Company: ${wtd.companyName}
+Exchange: ${wtd.proposedExchange}
+Shares: ${wtd.sharesOffered}
+Filled Date: ${wtd.filedDate}
+Offer Amount: ${wtd.dollarValueOfSharesOffered} `;
+        tweetID = await makeTweet(tweet, true, tweetID);
+        console.log(tweet);
+        break;
+    }
+
+}
+
+async function publicCompanies() {
+    const date = getDate();
+    const IPOs = await getIPOdata(date);
+    const withdrawns = await getWithdarawlsdata(date);
+    const whiteCompanies = [];
+
+    for (let IPO of IPOs) {
+        let proceed = isToday(IPO.expectedPriceDate);
+        if (proceed == true) {
+            console.log('Yes');
+            if (withdrawns.length > 0) {
+                for (let wtd of withdrawns) {
+                    if (wtd.companyName == IPO.companyName) {
+                        proceed = false;
+                    }
+                }
+            }
+            if (proceed == true) {
+                whiteCompanies.push(IPO);
+            }
+        }
+    }
+
+    if (whiteCompanies.length > 0) {
+        let tweet = `These companies are going public today!🧵\n\n`;
+        for (let i = 0; i < whiteCompanies.length; i++) {
+            tweet = tweet + `${i + 1}. ${whiteCompanies[0].companyName}`;
+        }
+        await makeTweet(tweet, false);
+        console.log(tweet);
+    }
+
+}
+
+async function getIPOdata(date) {
+    try {
+        var options = {
+            method: 'GET',
+            url: `https://api.nasdaq.com/api/ipo/calendar?date=${date}`,
+        };
+
+        let response = await axios.request(options);
+        console.log(response);
+        if (response.data.data.upcoming.upcomingTable.rows == null) { return []; }
+        return response.data.data.upcoming.upcomingTable.rows;
+    }
+    catch (e) {
+        return [];
+    }
+}
+
+async function getWithdarawlsdata(date) {
+    try {
+        var options = {
+            method: 'GET',
+            url: `https://api.nasdaq.com/api/ipo/calendar?date=${date}`,
+        };
+
+        let response = await axios.request(options);
+        console.log(response);
+        if (response.data.data.withdrawn.rows == null) { return []; }
+        return response.data.data.withdrawn.rows;
+    }
+    catch (e) {
+        return [];
+    }
+}
+
+async function makeTweet(tweet, isReply, tweetID) {
+    if (isReply == true) {
+        const reply = await client.v2.reply(tweet, tweetID);
+        console.log('Tweet Made: ' + JSON.stringify(reply));
+        return reply.data.id;
+    }
+    else {
+        const posted = await client.v2.tweet(tweet);
+        console.log('Tweet Made: ' + JSON.stringify(posted));
+        return posted.data.id;
+    }
+}
+
+function getDate() {
+    let date = new Date();
+    var offset = -300; //Timezone offset for EST in minutes.
+    var estDate = new Date(date.getTime() + offset * 60 * 1000);
+    estDate = estDate.toISOString();
+    estDate = estDate.split('-');
+    estDate = `${estDate[0]}-${estDate[1]} `;
+    return estDate;
+}
+
+function isToday(someDate) {
+
+    const date = new Date();
+    var offset = -300; //Timezone offset for EST in minutes.
+    var estDate = new Date(date.getTime() + offset * 60 * 1000);
+
+    estDate = estDate.toISOString().
+        replace(/T/, ' ').
+        replace(/\..+/, '')
+    estDate = estDate.split(' ');
+    estDate = estDate[0].split('-');
+    estDate = `${estDate[1]}/${estDate[2]}/${estDate[0]}`
+    if (estDate == someDate) {
+        return true;
+    }
+    return false;
+}
+
+function isLastDayOfMonth(date) {
+    return date.getDate() == new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate();
+}
